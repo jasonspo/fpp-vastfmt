f02ee21247f3551f00bba14377e3ca402100c662
Mat Mrosko <vastfmt@matmrosko.com>
2014-07-29 22:32:20 -0700

    [install] Use 4.7 if available for new C++11 rewrite of code.

--- fpp_install.sh
+++ fpp_install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+if [ -n "$(which g++-4.7)" ]; then
+	CXX=g++-4.7
+else
+	CXX=g++
+fi
+
 pushd $(dirname $(which $0))
 
 git submodule init
 git submodule update
-make -C vastfmt
+if [ "$CXX" == "g++" ]; then
+	# We don't support C++11 and need an old version of vastfmt
+	pushd vastfmt
+	git reset --hard 91b76c154d967ba22c5dc25831de6b888065ff87
+	popd
+fi
+make -C vastfmt CXX=$CXX
 mkdir -p bin
 cp vastfmt/radio bin/rds
 
