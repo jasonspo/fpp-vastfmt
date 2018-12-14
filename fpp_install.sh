749dc6bce52276a44e1223647809083947aa037d
Chris Pinkham <cpinkham@bc2va.org>
2018-12-14 11:01:54 -0800

    Update fpp-vastfmt to get it installing on FPP v2.x Stretch images.
    Also removed the jq submodule in favor of using apt-get install.

--- fpp_install.sh
+++ fpp_install.sh
@@ -1,38 +1,24 @@
 #!/bin/bash
 
-if [ -n "$(which g++-4.7)" ]; then
-	CXX=g++-4.7
-else
-	CXX=g++
-fi
-
 pushd $(dirname $(which $0))
 
 git submodule init
 git submodule update
-if [ "$CXX" == "g++" ]; then
-	# We don't support C++11 and need an old version of vastfmt
-	pushd vastfmt
-	git reset --hard 91b76c154d967ba22c5dc25831de6b888065ff87
-	popd
-fi
 
 # Before building, check that we have libusb-1.0-0-dev
 if ! dpkg --get-selections | grep -cq libusb-1.0-0-dev; then
-	$SUDO apt-get -q -y update
-	$SUDO apt-get -q -y --reinstall install libusb-1.0-0-dev
+	sudo apt-get -q -y update
+	sudo apt-get -q -y --reinstall install libusb-1.0-0-dev
 fi
 
-make -C vastfmt CXX=$CXX
+make -C vastfmt
+
 mkdir -p bin
 cp vastfmt/radio bin/rds
 
-# Get jq going for parsing our json data
-pushd jq
-autoreconf -i
-./configure --disable-maintainer-mode
-make src/builtin.inc
-make
-popd
+if ! dpkg --get-selections | grep -cq "^jq"; then
+	sudo apt-get -q -y update
+	sudo apt-get -q -y install jq
+fi
 
 popd
