950981b2aa34a89ab26e51161ae3d0a6ac0725ca
Daniel Kulp <dan@kulp.com>
2019-09-02 19:54:42 -0400

    Remove some old code

--- fpp_install.sh
+++ fpp_install.sh
@@ -2,23 +2,10 @@
 
 pushd $(dirname $(which $0))
 
-git submodule init
-git submodule update
-
 # Before building, check that we have libusb-1.0-0-dev
 if ! dpkg --get-selections | grep -cq libusb-1.0-0-dev; then
 	sudo apt-get -q -y update
 	sudo apt-get -q -y --reinstall install libusb-1.0-0-dev
-fi
-
-make -C vastfmt
-
-mkdir -p bin
-cp vastfmt/radio bin/rds
-
-if ! dpkg --get-selections | grep -cq "^jq"; then
-	sudo apt-get -q -y update
-	sudo apt-get -q -y install jq
 fi
 
 popd
